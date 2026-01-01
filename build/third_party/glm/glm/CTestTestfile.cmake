aee6325dbc7b47aac7af42d22e97831ec047ef90
刘砚桐 <549138947@qq.com>
2026-01-01 15:36:04 +0800

    chore: add .gitignore and remove build artifacts from tracking

--- build/third_party/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/workspace/fluid-sim-comment-master/code/third_party/glm/glm
-# Build directory: E:/workspace/fluid-sim-comment-master/code/build/third_party/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
